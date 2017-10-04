d6a74d587512c73d255e0ef15f06416e907c8a7c
Jacob Hamberg <halbridious@gmail.com>
2017-10-04 17:22:51 -0400

    Initial thing from class

---
+++ Assets/Camera_rayCaster.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_rayCaster : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject prefab;
+
+    [SerializeField]
+    private LayerMask layerMask;
+
+    [SerializeField]
+    private GameObject cylindar;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if( Input.GetButtonDown("Fire1") ) DoRayCast();
+
+	}
+
+    private void DoRayCast() {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        Debug.DrawRay(ray.origin, ray.direction * 10, Color.yellow, 5);
+        if( Physics.Raycast(ray, out hit, 10, layerMask) ) {
+            //hit.collider.GetComponent<MeshRenderer>().material.color = Color.red;
+
+            Quaternion rot = Quaternion.FromToRotation(Vector3.up, hit.normal);
+            cylindar.transform.position = hit.point;
+            cylindar.transform.rotation = rot;
+            //Instantiate(prefab, hit.point, rot);
+
+        }
+        
+
+
+    }
+
+}
